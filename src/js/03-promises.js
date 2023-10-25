839886d4624bbd9051a56c996f1f5fa2411acf63
andyfullstack <andriikoliadenko@gmail.com>
2023-10-25 16:52:12 +0300

    clockers done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+function makethePromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const isResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (isResolve) {
+       
+        resolve({ position, delay });
+      } else {
+        
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const firstDelay = parseInt(evt.target.elements.delay.value);
+  const stepDelay = parseInt(evt.target.elements.step.value);
+  const total = parseInt(evt.target.elements.amount.value);
+
+  for (let i = 0; i < total; i++) {
+    const currentDelay = firstDelay + i * stepDelay;
+    makethePromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
